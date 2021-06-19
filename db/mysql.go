62f480e6b35d3f3158761a188d176b134578bbbd
changlin93 <767425412@qq.com>
2021-06-19 18:22:58 +0800

    feat: 处理数据库链接相关逻辑

---
+++ db/mysql.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"blog/conf"
+	"blog/utils"
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+var Db *gorm.DB
+
+func init() {
+	var err error
+	var dbConfig = conf.Conf.Db
+	url := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=True&loc=Local",
+		dbConfig.Username,
+		dbConfig.Password,
+		dbConfig.Host,
+		dbConfig.Port,
+		dbConfig.Db,
+		dbConfig.Charset)
+
+	Db, err := gorm.Open(conf.Conf.Db.Dialects, url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if Db.Error != nil {
+		panic(Db.Error)
+	}
+
+	Db.DB().SetMaxIdleConns(dbConfig.MaxIdle)
+	Db.DB().SetMaxOpenConns(dbConfig.MaxOpen)
+	logger := utils.Log()
+	Db.SetLogger(logger)
+	Db.LogMode(true)
+	logger.Info("mysql connect success")
+
+}
